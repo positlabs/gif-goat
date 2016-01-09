106eb76b9dff89b3c587b05bd2c8dc6515c0364a
Josh Beckwith <josh.beckwith@gmail.com>
2016-01-08 18:02:51 -0800

    working on #4

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 'use strict';
 const electron = require('electron');
-const app = electron.app;  // Module to control application life.
-const BrowserWindow = electron.BrowserWindow;  // Module to create native browser window.
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
 const ipc = electron.ipcMain;
 const dialog = require('electron').dialog;
 const argv = require('yargs').argv;
@@ -10,37 +10,32 @@ const argv = require('yargs').argv;
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
-
 let previews = [];
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function() {
+// app.on('window-all-closed', function() {
 	
-});
+// });
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
 app.on('ready', function() {
-	// Create the browser window.
+
 	mainWindow = new BrowserWindow({
 		width: 400, 
 		height: 600,
-		// title: 'GIFFLY',
 		minWidth: 400, 
 		minHeight: 520,
 		darkTheme: true,
 		useContentSize: true //The width and height would be used as web page's size, which means the actual window's size will include window frame's size and be slightly larger.
 	});
 
-	// and load the index.html of the app.
 	mainWindow.loadURL('file://' + __dirname + '/index.html');
 
+	// TODO: use NODE_ENV
 	// Open the DevTools.
-	if(argv.debug){ // only do this if we're in dev mode
+	// if(argv.debug){ // only do this if we're in dev mode
 		mainWindow.webContents.openDevTools();
-	}
+	// }
 
-	// Emitted when the window is closed.
 	mainWindow.on('closed', function() {
 		// Dereference the window object, usually you would store windows
 		// in an array if your app supports multi windows, this is the time
